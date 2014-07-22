2278d8a8fbebc058c59cf7aa05bf3b36ec7336ea
sheeksav <andy.sheeks@gmail.com>
2014-07-21 22:10:42 -0400

    Added a currency filter.

--- js/app.js
+++ js/app.js
@@ -12,7 +12,7 @@
     var gems = [
         {
             name: 'Dodecahedron',
-            price: 2.95,
+            price: 2.00,
             description: 'In geometry, a dodecahedron (Greek δωδεκάεδρον, from δώδεκα, dōdeka "twelve" + ἕδρα hédra "base", "seat" or "face") is any polyhedron with twelve flat faces, but usually a regular dodecahedron is meant: a Platonic solid. It is composed of 12 regular pentagonal faces, with three meeting at each vertex, and is represented by the Schläfli symbol {5,3}. It has 20 vertices, 30 edges and 160 diagonals. Its dual polyhedron is the icosahedron, with Schläfli symbol {3,5}.',
             canPurchase: true,
         },
